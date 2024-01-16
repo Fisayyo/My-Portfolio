e9ed3a1724536c652cc35e2799e415600e7ae346
Olaleye Oluwafisayo Ernest <107893269+Fisayyo@users.noreply.github.com>
2024-01-16 13:57:40 -0400

    Update data.js

--- my-portfolio/src/data.js
+++ my-portfolio/src/data.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "React Reserve",
     subtitle: "MERN Stack",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "A bunch of projects i have either doen for people or companies ",
     image: "./project-1.gif",
     link: "https://reactbootcamp.com",
   },
